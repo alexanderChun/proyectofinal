fd396b8cf1844127c7cdc1e1efa5ea84bcae37c2
Edwin Alexander Chun <Edwin.Chun@naturaceites.com>
2021-05-29 11:32:16 -0600

    ¨Actualizado

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -26,31 +26,31 @@
         @csrf
 
         <div class="form-group">
-            <label>Name</label>
+            <label>Nombre</label>
             <input type="text" class="form-control" name="name" id="name">
         </div>
 
         <div class="form-group">
-            <label>Email</label>
+            <label>Correo Electronico</label>
             <input type="email" class="form-control" name="email" id="email">
         </div>
 
         <div class="form-group">
-            <label>Phone</label>
+            <label>Teléfono</label>
             <input type="text" class="form-control" name="phone" id="phone">
         </div>
 
         <div class="form-group">
-            <label>Subject</label>
+            <label>Asunto</label>
             <input type="text" class="form-control" name="subject" id="subject">
         </div>
 
         <div class="form-group">
-            <label>Message</label>
+            <label>Mensaje</label>
             <textarea class="form-control" name="message" id="message" rows="4"></textarea>
         </div>
 
-        <input type="submit" name="send" value="Submit" class="btn btn-dark btn-block">
+        <input class="btn btn-primary" type="submit" value="Enviar">
     </form>
 </div>
 </body>
